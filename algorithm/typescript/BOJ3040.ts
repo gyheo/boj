0a22dd592148cdf6e3fb50fcbe112ca796c19f20
Heo GeonYeong <gyheo@icloud.com>
2021-12-27 23:59:15 +0900

    Edit BOJ3040.ts

--- algorithm/typescript/BOJ3040.ts
+++ algorithm/typescript/BOJ3040.ts
@@ -1,30 +1,30 @@
 // from https://www.acmicpc.net/problem/3040
 
-import fs = require('fs');
+import fs = require("fs");
 
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 const result = Array(7).fill(0);
 
 function selectDwarf(cnt: number, start: number): void {
-	if (cnt == 7) {
-		let sum = 0;
-		for (let i = 0; i < 7; i++) {
-			sum += result[i];
-		}
+  if (cnt == 7) {
+    let sum = 0;
+    for (let i = 0; i < 7; i++) {
+      sum += result[i];
+    }
 
-		if (sum == 100) {
-			for (let i = 0; i < 7; i++) {
-				console.log(result[i]);
-			}
-		}
+    if (sum == 100) {
+      for (let i = 0; i < 7; i++) {
+        console.log(result[i]);
+      }
+    }
 
-		return;
-	}
+    return;
+  }
 
-	for (let i = start; i < 9; i++) {
-		result[cnt] = Number(input[i]);
-		selectDwarf(cnt + 1, i + 1);
-	}
+  for (let i = start; i < 9; i++) {
+    result[cnt] = Number(input[i]); // 숫자 형변환
+    selectDwarf(cnt + 1, i + 1);
+  }
 }
 
 selectDwarf(0, 0);
