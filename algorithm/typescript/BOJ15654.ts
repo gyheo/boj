efc218966240787bdf277382fa1f81be7e0cc0d8
Heo GeonYeong <gyheo@icloud.com>
2021-12-27 23:56:57 +0900

    Edit BOJ15654.ts

--- algorithm/typescript/BOJ15654.ts
+++ algorithm/typescript/BOJ15654.ts
@@ -14,21 +14,21 @@ const result = [];
 const isSelected = Array(N).fill(false);
 
 function permutation(cnt: number): void {
-	if (cnt == M) {
-		for (let i = 0; i < M; i++) {
-			result.push(`${output[i]} `);
-		}
-		result.push(`\n`);
-		return;
-	}
+  if (cnt == M) {
+    for (let i = 0; i < M; i++) {
+      result.push(`${output[i]} `);
+    }
+    result.push(`\n`);
+    return;
+  }
 
-	for (let i = 0; i < N; i++) {
-		if (isSelected[i]) continue;
-		isSelected[i] = true;
-		output[cnt] = input[i];
-		permutation(cnt + 1);
-		isSelected[i] = false;
-	}
+  for (let i = 0; i < N; i++) {
+    if (isSelected[i]) continue;
+    isSelected[i] = true;
+    output[cnt] = input[i];
+    permutation(cnt + 1);
+    isSelected[i] = false;
+  }
 }
 
 permutation(0);
