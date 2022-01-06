cebe57ef5015229871dc6331d3014ba72e99bbff
Heo GeonYeong <gyheo@icloud.com>
2022-01-06 23:36:29 +0900

    Add BOJ1764.py

---
+++ algorithm/python/BOJ1764.py
@@ -0,0 +1,27 @@
+# problem : https://www.acmicpc.net/problem/1764
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+name_dict = dict()
+result = list()
+
+for i in range(0, N):
+    name = sys.stdin.readline().rstrip()
+    name_dict[name] = 1
+
+for i in range(0, M):
+    name = sys.stdin.readline().rstrip()
+    if name_dict.get(name) != None:
+        name_dict[name] = name_dict.get(name) + 1
+
+        if name_dict[name] >= 2:
+            result.append(name)
+
+    else:
+        name_dict[name] = 1
+
+result.sort()
+print(len(result))
+for name in result:
+    print(name)
