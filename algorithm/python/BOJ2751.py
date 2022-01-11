c589f90fa955251ce98facddf7ba5053329c0386
Heo GeonYeong <gyheo@icloud.com>
2022-01-11 23:55:32 +0900

    Add BOJ2751.py

---
+++ algorithm/python/BOJ2751.py
@@ -0,0 +1,13 @@
+# problem : https://www.acmicpc.net/problem/2751
+
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+arr = list()
+for i in range(0, N):
+    arr.append(int(sys.stdin.readline().rstrip()))
+
+arr.sort()
+
+for i in range(0, N):
+    print(arr[i])
