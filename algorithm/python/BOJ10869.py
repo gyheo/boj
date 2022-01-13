4c893be7c664fc951d97e1f2e0ddc90f809c49ae
gyheo <gyheo@icloud.com>
2022-01-13 23:53:04 +0900

    Add BOJ10869.py

---
+++ algorithm/python/BOJ10869.py
@@ -0,0 +1,11 @@
+# problem : https://www.acmicpc.net/problem/10869
+
+import sys
+
+A, B = map(int, sys.stdin.readline().rstrip().split())
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
