1668f61c8a60d8b0982194792dcdd2601aaed0c7
gyheo <gyheo@icloud.com>
2022-01-25 23:35:16 +0900

    Edit BOJ10950.java

--- algorithm/java/src/BOJ10950.java
+++ algorithm/java/src/BOJ10950.java
@@ -1,17 +1,25 @@
-import java.util.Scanner;
+// problem : https://www.acmicpc.net/problem/10950
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ10950 {
     static int A, B;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
         for (int tc = 1; tc <= T; tc++) {
-            A = sc.nextInt();
-            B = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+
             sb.append(A + B).append("\n");
         }
+
         sb.setLength(sb.length() - 1);
         System.out.println(sb);
     }
