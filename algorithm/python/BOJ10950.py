cdcd7c9feb866b5e183c7cea4995fe09b6dfb339
gyheo <gyheo@icloud.com>
2022-01-25 23:35:25 +0900

    Edit BOJ10950.py

--- algorithm/python/BOJ10950.py
+++ algorithm/python/BOJ10950.py
@@ -1,8 +1,12 @@
-T = int(input())
+# problem : https://www.acmicpc.net/problem/10950
+
+import sys
+
+T = int(sys.stdin.readline().rstrip())
 
 answer = []
 for i in range(0, T):
-    A, B = map(int, input().split())
+    A, B = map(int, sys.stdin.readline().rstrip().split())
     answer.append(A + B)
 
 for ans in answer:
