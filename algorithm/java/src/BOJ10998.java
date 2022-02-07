2652851db2a577fbe47b1056b88c21da657fb1ea
gyheo <gyheo@icloud.com>
2022-02-07 23:49:15 +0900

    Add BOJ10998.java

---
+++ algorithm/java/src/BOJ10998.java
@@ -0,0 +1,19 @@
+// problem : https://www.acmicpc.net/problem/10998
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10998 {
+    static int A, B;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A * B);
+    }
+}
